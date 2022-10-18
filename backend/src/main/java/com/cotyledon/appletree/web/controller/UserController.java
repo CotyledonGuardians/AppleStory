86310364b309aab409d036ddd6dd44aa16cfbeb9
Seona Song <seona0667@naver.com>
2022-10-18 13:07:18 +0900

    refactor: 💡 로직 변경

--- backend/src/main/java/com/cotyledon/appletree/web/controller/UserController.java
+++ backend/src/main/java/com/cotyledon/appletree/web/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.cotyledon.appletree.web.controller;
 
-import com.cotyledon.appletree.web.request.UserRegisterRequest;
+import com.cotyledon.appletree.common.util.BaseResponse;
+import com.cotyledon.appletree.domain.dto.UserDTO;
 import com.cotyledon.appletree.web.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
@@ -17,7 +17,12 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping()
-    public void register(@RequestBody UserRegisterRequest userRegisterRequest){
-        userService.register(userRegisterRequest);
+    public ResponseEntity<?> register(@RequestBody UserDTO userDTO){
+        try {
+            userService.register(userDTO);
+        } catch (Exception e){
+            return BaseResponse.fail(e.getMessage());
+        }
+        return BaseResponse.success();
     }
 }
