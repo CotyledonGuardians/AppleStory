2a0d47a676b2db8d694feb95eee07f04facdfecb
차송희 <chasonghui@naver.com>
2022-10-20 14:36:06 +0900

    feat: 🎸 create folder structure, screans

--- AppleTree/index.js
+++ AppleTree/index.js
@@ -4,6 +4,10 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Main from './screens/Main';
+import Map from './screens/Map';
+import AppleList from './screens/AppleList';
+import MyPage from './screens/auth/MyPage';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Main);
