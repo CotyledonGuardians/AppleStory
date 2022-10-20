802586f6131d95bc0d56f2b990708c968849664f
Seona Song <seona0667@naver.com>
2022-10-20 15:58:13 +0900

    feat: 🎸 create swagger

---
+++ backend/src/main/java/com/cotyledon/appletree/configuration/SwaggerConfiguration.java
@@ -0,0 +1,71 @@
+package com.cotyledon.appletree.configuration;
+
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfiguration {
+
+    // http://localhost:8080/swagger-ui/index.html
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+//                .useDefaultResponseMessages(false)
+//                .apiInfo(this.apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.cotyledon.appletree.web.controller"))
+//                .paths(PathSelectors.any())
+//                .build();
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .apiInfo(this.apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.cotyledon.appletree.web.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                ;
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Cotyledon Project")
+                .description("Authorize 에 Bearer [accessToken]을 입력후 실행")
+                .build();
+    }
+
+
+}
