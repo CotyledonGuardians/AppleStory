e9e50e58270fc0bc7e4b585de7d4b0b3db4da965
Seona Song <seona0667@naver.com>
2022-10-25 13:38:42 +0900

    feat: 🎸 apple List

--- AppleTree/screens/AppleList.js
+++ AppleTree/screens/AppleList.js
@@ -1,9 +1,222 @@
 import * as React from 'react';
-import {SafeAreaView, StyleSheet, Text, View, Image} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import SelectDropdown from 'react-native-select-dropdown';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const data = [
+  {
+    type: false,
+    title: '테스트임1!',
+    creator: {
+      teamName: '테스트팀',
+      hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+      member: [
+        {
+          nickname: '테스트닉네임',
+          uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+        },
+      ],
+    },
+    createAt: '2022-10-15T00:00:00.000+00:00',
+    unlockAt: '2024-10-14T00:00:00.000+00:00',
+    createScene: 'https://www.google.com',
+    content: null,
+    location: null,
+    useSpace: false,
+    uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+    isShow: false,
+    isOpen: false,
+  },
+  {
+    type: true,
+    title: '테스트임1!',
+    creator: {
+      teamName: '테스트팀',
+      hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+      member: [
+        {
+          nickname: '테스트닉네임',
+          uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+        },
+      ],
+    },
+    createAt: '2022-10-15T00:00:00.000+00:00',
+    unlockAt: '2022-10-16T00:00:00.000+00:00',
+    createScene: 'https://www.google.com',
+    content: null,
+    location: null,
+    useSpace: false,
+    uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+    isShow: false,
+    isOpen: false,
+  },
+  {
+    type: false,
+    title: '테스트임1!',
+    creator: {
+      teamName: '테스트팀',
+      hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+      member: [
+        {
+          nickname: '테스트닉네임',
+          uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+        },
+      ],
+    },
+    createAt: '2022-10-15T00:00:00.000+00:00',
+    unlockAt: '2024-10-14T00:00:00.000+00:00',
+    createScene: 'https://www.google.com',
+    content: null,
+    location: null,
+    useSpace: false,
+    uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+    isShow: false,
+    isOpen: false,
+  },
+  {
+    type: false,
+    title: '테스트임1!',
+    creator: {
+      teamName: '테스트팀',
+      hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+      member: [
+        {
+          nickname: '테스트닉네임',
+          uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+        },
+      ],
+    },
+    createAt: '2022-10-15T00:00:00.000+00:00',
+    unlockAt: '2024-10-14T00:00:00.000+00:00',
+    createScene: 'https://www.google.com',
+    content: null,
+    location: null,
+    useSpace: false,
+    uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+    isShow: false,
+    isOpen: false,
+  },
+  {
+    type: false,
+    title: '테스트임1!',
+    creator: {
+      teamName: '테스트팀',
+      hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+      member: [
+        {
+          nickname: '테스트닉네임',
+          uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+        },
+      ],
+    },
+    createAt: '2022-10-15T00:00:00.000+00:00',
+    unlockAt: '2022-10-14T00:00:00.000+00:00',
+    createScene: 'https://www.google.com',
+    content: null,
+    location: null,
+    useSpace: false,
+    uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+    isShow: false,
+    isOpen: false,
+  },
+  {
+    type: false,
+    title: '테스트임1!',
+    creator: {
+      teamName: '테스트팀',
+      hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+      member: [
+        {
+          nickname: '테스트닉네임',
+          uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+        },
+      ],
+    },
+    createAt: '2022-10-15T00:00:00.000+00:00',
+    unlockAt: '2022-10-14T00:00:00.000+00:00',
+    createScene: 'https://www.google.com',
+    content: null,
+    location: null,
+    useSpace: false,
+    uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+    isShow: false,
+    isOpen: false,
+  },
+  {
+    type: true,
+    title: '테스트임1!',
+    creator: {
+      teamName: '테스트팀',
+      hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+      member: [
+        {
+          nickname: '테스트닉네임',
+          uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+        },
+      ],
+    },
+    createAt: '2022-10-15T00:00:00.000+00:00',
+    unlockAt: '2024-10-22T00:00:00.000+00:00',
+    createScene: 'https://www.google.com',
+    content: null,
+    location: null,
+    useSpace: false,
+    uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
+    isShow: false,
+    isOpen: true,
+  },
+];
+
+var randomgroupImages = [
+  require('../assets/pictures/listgroup1.png'),
+  require('../assets/pictures/listgroup2.png'),
+];
+var randomhitgroupImages = [
+  require('../assets/pictures/readyhitgroup1.png'),
+  require('../assets/pictures/readyhitgroup2.png'),
+];
+
+const Card = ({type, title, unlockAt, isOpen, index}: any) => {
+  let lockDate = new Date(unlockAt);
+  let today = new Date();
+  var url = '';
+
+  if (!isOpen && today >= lockDate) {
+    url =
+      randomhitgroupImages[
+        Math.floor(Math.random() * randomhitgroupImages.length)
+      ];
+  } else {
+    if (type) {
+      url = require('../assets/pictures/listpersonal1.png');
+    } else {
+      url =
+        randomgroupImages[Math.floor(Math.random() * randomgroupImages.length)];
+    }
+  }
+
+  return (
+    <View style={styles.card}>
+      <Image
+        source={url}
+        style={{width: '70%', height: '70%'}}
+        resizeMode="contain"
+      />
+      <Text style={{fontFamily: 'UhBee Se_hyun', color: '#4C4036'}}>
+        {title}
+      </Text>
+    </View>
+  );
+};
 
 function DropdownSelect() {
   const countries = [
@@ -29,16 +242,6 @@ function DropdownSelect() {
       }}
       buttonStyle={styles.dropdown2BtnStyle}
       buttonTextStyle={styles.dropdown2BtnTxtStyle}
-      // renderDropdownIcon={isOpened => {
-      //   return (
-      //     <FontAwesome
-      //       name={isOpened ? 'chevron-up' : 'chevron-down'}
-      //       color={'#000'}
-      //       size={30}
-      //     />
-      //   );
-      // }}
-      // dropdownIconPosition={'right'}
       dropdownStyle={styles.dropdown2DropdownStyle}
       rowStyle={styles.dropdown2RowStyle}
       rowTextStyle={styles.dropdown2RowTxtStyle}
@@ -47,143 +250,6 @@ function DropdownSelect() {
 }
 
 function HomeScreen() {
-  var myloop = [];
-  var arr = [
-    {
-      type: false,
-      title: '테스트임1!',
-      creator: {
-        teamName: '테스트팀',
-        hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-        member: [
-          {
-            nickname: '테스트닉네임',
-            uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-          },
-        ],
-      },
-      createAt: '2022-10-15T00:00:00.000+00:00',
-      unlockAt: '2022-10-16T00:00:00.000+00:00',
-      createScene: 'https://www.google.com',
-      content: null,
-      location: null,
-      useSpace: false,
-      uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-      isShow: false,
-      isOpen: false,
-    },
-    {
-      type: false,
-      title: '테스트임2!',
-      creator: {
-        teamName: '테스트팀',
-        hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-        member: [
-          {
-            nickname: '테스트닉네임',
-            uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-          },
-        ],
-      },
-    },
-    {
-      type: false,
-      title: '테스트임3!',
-      creator: {
-        teamName: '테스트팀',
-        hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-        member: [
-          {
-            nickname: '테스트닉네임',
-            uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-          },
-          {
-            nickname: '테스트닉네임',
-            uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-          },
-        ],
-      },
-    },
-    {
-      type: false,
-      title: '테스트임4!',
-      creator: {
-        teamName: '테스트팀',
-        hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-        member: [
-          {
-            nickname: '테스트닉네임',
-            uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-          },
-        ],
-      },
-    },
-    {
-      type: false,
-      title: '테스트임5!',
-      creator: {
-        teamName: '테스트팀',
-        hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-        member: [
-          {
-            nickname: '테스트닉네임',
-            uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-          },
-        ],
-      },
-    },
-    {
-      type: false,
-      title: '테스트임6!',
-      creator: {
-        teamName: '테스트팀',
-        hostUid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-        member: [
-          {
-            nickname: '테스트닉네임',
-            uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-          },
-          {
-            nickname: '테스트닉네임',
-            uid: 'WQk7cwPRUYhcaW4iUT9ZWZz8nil2',
-          },
-        ],
-      },
-    },
-  ];
-  for (let i = 0; i < arr.length; i++) {
-    // 사람 개수
-    // console.log(arr[i].creator.member.length);
-    // console.log(arr[i].title);
-
-    //Math.random() * 10) + 1;
-    var count = Math.random();
-    var listPerson = '../assets/pictures/listpersona' + count + '.png';
-    console.log(listPerson);
-    myloop.push(
-      <View style={styles.box}>
-        <View style={styles.inner}>
-          <Image
-            source={
-              arr[i].creator.member.length == 1
-                ? require('../assets/pictures/listpersonal1.png')
-                : require('../assets/pictures/listgroup1.png')
-            }
-            style={{width: '70%', height: '70%'}}
-            resizeMode="contain"
-          />
-          <Text>{arr[i].title}</Text>
-        </View>
-      </View>,
-    );
-  }
-  // for (let i = 0; i < 10; i++) {
-  //   myloop.push(
-  //     <View key={i}>
-  //       <Text>{i}</Text>
-  //     </View>,
-  //   );
-  // }
   return (
     <View
       style={{
@@ -192,9 +258,29 @@ function HomeScreen() {
         alignItems: 'center',
         backgroundColor: '#FBF8F6',
       }}>
-      <DropdownSelect />
-      <View style={styles.boxContainer}>{myloop}</View>
-      {/* <Text>잠긴 사과</Text> */}
+      <View style={{height: 100, alignItems: 'center'}}>
+        <DropdownSelect />
+      </View>
+      <ScrollView
+        contentContainerStyle={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}>
+        {data.map((item, index) => {
+          //   console.log(index);
+          return (
+            <View style={{width: '50%', flexDirection: 'row'}}>
+              <Card
+                key={index}
+                title={item.title}
+                type={item.type}
+                unlockAt={item.unlockAt}
+                isOpen={item.isOpen}
+              />
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -208,8 +294,29 @@ function SettingsScreen() {
         alignItems: 'center',
         backgroundColor: '#FBF8F6',
       }}>
-      <DropdownSelect />
-      <Text>열린 사과</Text>
+      <View style={{height: 90, alignItems: 'center'}}>
+        <DropdownSelect />
+      </View>
+      <ScrollView
+        contentContainerStyle={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}>
+        {data.map((item, index) => {
+          //   console.log(index);
+          return (
+            <View style={{width: '50%', flexDirection: 'row'}}>
+              <Card
+                key={index}
+                title={item.title}
+                type={item.type}
+                unlockAt={item.unlockAt}
+                isOpen={item.isOpen}
+              />
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -243,6 +350,13 @@ const AppleList = () => {
 };
 
 const styles = StyleSheet.create({
+  card: {
+    height: 200,
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   dropdown2BtnStyle: {
     width: '50%',
     height: 50,
@@ -255,6 +369,7 @@ const styles = StyleSheet.create({
     color: '#4C4036',
     textAlign: 'center',
     fontWeight: 'bold',
+    fontFamily: 'UhBee Se_hyun',
   },
   dropdown2DropdownStyle: {
     backgroundColor: '#ECE5E0',
@@ -266,20 +381,13 @@ const styles = StyleSheet.create({
     color: '#4C4036',
     textAlign: 'center',
     fontWeight: 'bold',
+    fontFamily: 'UhBee Se_hyun',
   },
   boxContainer: {
     width: '100%',
     height: '70%',
     flexDirection: 'row',
     flexWrap: 'wrap',
-  },
-  box: {
-    width: '50%',
-    height: '50%',
-  },
-  inner: {
-    flex: 1,
-    alignItems: 'center',
   },
 });
 
