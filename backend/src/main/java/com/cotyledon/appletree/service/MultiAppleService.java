fe7135973b618bcdfcd61f4a215e4ea9242241b5
송제영 <wp29dud@naver.com>
2022-11-01 00:36:00 +0900

    fix: 🐛 fix duplicated content author issue when added
    Former-commit-id: f49cf91e09e97a0175f69ceeeaa3c9258ae496a8

--- backend/src/main/java/com/cotyledon/appletree/service/MultiAppleService.java
+++ backend/src/main/java/com/cotyledon/appletree/service/MultiAppleService.java
@@ -2,11 +2,11 @@ package com.cotyledon.appletree.service;
 
 import com.cotyledon.appletree.domain.dto.AppleDTO;
 
-import java.util.List;
+import java.util.Set;
 
 public interface MultiAppleService {
 
-    void saveAppleAndAppleUsers(AppleDTO appleDTO, List<String> userUids);
+    Long saveAppleAndAppleUsersAndGetAppleId(AppleDTO appleDTO, Set<String> userUids);
     Long reserveAppleAndGetId();
     void deleteAppleIfEmpty(Long appleId);
 }
