f5c08129e12523c4d34d478150fbca452fcc3df4
Seona Song <seona0667@naver.com>
2022-11-17 09:50:57 +0900

    feat: 🎸 image download
    Former-commit-id: 3d801b7304d106d0749fb6a146d53eab91303532

--- AppleTree/screens/unlock/ImageFullScreen.js
+++ AppleTree/screens/unlock/ImageFullScreen.js
@@ -1,16 +1,109 @@
-import React, {useState} from 'react';
-import {StyleSheet, Image} from 'react-native';
+import React from 'react';
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  Platform,
+  TouchableOpacity,
+  PermissionsAndroid,
+} from 'react-native';
+import RNFetchBlob from 'rn-fetch-blob';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 const ImageFullScreen = ({route}) => {
-  const url = route.params.url;
+  const fileUrl = route.params.url;
+  const checkPermission = async () => {
+    if (Platform.OS === 'ios') {
+      downloadFile();
+    } else {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          {
+            title: 'Storage Permission Required',
+            message:
+              'Application needs access to your storage to download File',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          // Start downloading
+          downloadFile();
+          console.log('Storage Permission Granted.');
+        } else {
+          // If permission denied then show alert
+          Alert.alert('Error', 'Storage Permission Not Granted');
+        }
+      } catch (err) {
+        // To handle permission related exception
+        console.log('++++' + err);
+      }
+    }
+  };
+
+  const downloadFile = () => {
+    // Get today's date to add the time suffix in filename
+    let date = new Date();
+    // File URL which we want to download
+    let FILE_URL = fileUrl;
+    // Function to get extention of the file url
+    let file_ext = getFileExtention(FILE_URL);
+
+    file_ext = '.' + file_ext[0];
+
+    // config: To get response by passing the downloading related options
+    // fs: Root directory path to download
+    const {config, fs} = RNFetchBlob;
+    let RootDir = fs.dirs.PictureDir;
+    let options = {
+      fileCache: true,
+      addAndroidDownloads: {
+        path:
+          RootDir +
+          '/file_' +
+          Math.floor(date.getTime() + date.getSeconds() / 2) +
+          file_ext,
+        description: 'downloading file...',
+        notification: true,
+        // useDownloadManager works with Android only
+        useDownloadManager: true,
+      },
+    };
+    config(options)
+      .fetch('GET', FILE_URL)
+      .then(res => {
+        // Alert after successful downloading
+        console.log('res -> ', JSON.stringify(res));
+        alert('File Downloaded Successfully.');
+      });
+  };
+
+  const getFileExtention = fileUrl => {
+    // To get the file extension
+    return /[.]/.exec(fileUrl) ? /[^.]+$/.exec(fileUrl) : undefined;
+  };
 
   return (
-    <Image
-      style={styles.image}
-      source={{
-        uri: url,
-      }}
-    />
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: fileUrl,
+        }}
+        style={{
+          resizeMode: 'contain',
+          width: wp('100%'),
+          height: hp('80%'),
+          //   aspectRatio: 1.5,
+          flex: 1,
+        }}
+      />
+      <TouchableOpacity style={styles.button} onPress={checkPermission}>
+        <Text style={styles.text}>Download File</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -20,9 +113,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FBF8F6',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  image: {
-    flex: 1,
-    resizeMode: 'contain',
+  text: {
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+    padding: 5,
+  },
+  button: {
+    width: '80%',
+    padding: 10,
+    backgroundColor: 'blue',
+    margin: 10,
   },
 });
